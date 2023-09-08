801636ee80818bf9a41d96a6a017a3ac5fd31997
Mahasin91 <mahasinmoawad@gmail.com>
2023-09-08 19:36:03 -0400

    ¨c¨

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: base 10 numbers
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int d = 0;
+while (d <= 9)
+{
+putchar(d + '0');
+d++;
+}
+putchar('\n')
+return (0);
+}
