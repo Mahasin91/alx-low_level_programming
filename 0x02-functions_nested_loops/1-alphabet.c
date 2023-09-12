51bc1696d107a042bb2949e7a2a30742970e1f6e
Mahasin91 <mahasinmoawad@gmail.com>
2023-09-12 19:27:38 -0400

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: print alphabet using putchar prototype
+*/
+
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a'; ch < 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
