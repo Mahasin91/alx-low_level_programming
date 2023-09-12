c9ef048c26f4235e69d04f141606de8ef41f8f6c
Mahasin91 <mahasinmoawad@gmail.com>
2023-09-12 19:52:08 -0400

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+int ch, line;
+for (line = 0; line <= 9; line++)
+{
+for (ch = 'a'; ch < 'z'; ch++)
+_putchar(str[ch]);
+_putchar('\n');
+}
+}
