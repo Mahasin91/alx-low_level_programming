e2ce416ce87b30246d0eac7d8ba551f043c79fc6
Mahasin91 <mahasinmoawad@gmail.com>
2023-09-14 19:00:24 -0400

    c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * print_numbers - print integers
+ * print_numbers - print integers only using _putchar twice
  * Return: always 0 (success)
 */
 
@@ -11,5 +11,5 @@ do {
 _putchar(num + 48);
 num++;
 } while (num >= 0 && num <= 9);
-_putchar("\n")
+_putchar("\n");
 }
