da576035261e89a416824db50944c632c94c0b48
Mahasin91 <mahasinmoawad@gmail.com>
2023-09-14 20:42:20 -0400

    c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * print_triangle - print line diagonal triangle
+ * @size: size of triangle
+ * Return: Always 0 (success)
+*/
+
+void print_triangle(int size)
+{
+int hight, base;
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (hight = 1; hight <= size; hight++)
+{
+for (base = 1; base <= hight; base++)
+{
+if ((hight + base) <= size)
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
