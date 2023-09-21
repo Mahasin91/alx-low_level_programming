de6861b9804d6a7773f46172342b1696369ee840
Mahasin91 <mahasinmoawad@gmail.com>
2023-09-20 21:48:32 -0400

    c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_line - prints a buffer
- * @c: buffer
- * @s: bytes of buffer
- * @l: line of buffer
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
  *
  * Return: void
  */
 
 void print_line(char *c, int s, int l)
 {
-int i, k;
-for (i = 0; i <= 9; i++)
+int j, k;
+for (j = 0; j <= 9; j++)
 {
-if (i <= s)
-printf("%02x", c[l * 10 + i]);
+if (j <= s)
+printf("%02x", c[l * 10 + j]);
 else
 printf("  ");
-if (i % 2)
+if (j % 2)
 putchar(' ');
 }
 for (k = 0; k <= s; k++)
@@ -32,23 +32,24 @@ putchar('.');
 
 /**
  * print_buffer - prints a buffer
- * @b: buffer
+ * @b: buffer to print
  * @size: size of buffer
+ *
  * Return: void
  */
 void print_buffer(char *b, int size)
 {
-int j;
-for (j = 0; j <= (size - 1) / 10 && size; j++)
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
 {
-printf("%08x: ", j * 10);
-if (j < size / 10)
+printf("%08x: ", i * 10);
+if (i < size / 10)
 {
-print_line(b, 9, j);
+print_line(b, 9, i);
 }
 else
 {
-print_line(b, size % 10 - 1, j);
+print_line(b, size % 10 - 1, i);
 }
 putchar('\n');
 }
