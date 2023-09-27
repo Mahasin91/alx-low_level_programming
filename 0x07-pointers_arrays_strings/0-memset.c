5bb0e583fb9072d8cfba29ce4b8c4887fb3eb463
Mahasin91 <mahasinmoawad@gmail.com>
2023-09-27 13:38:20 -0400

    c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memset - file memory with constant byte
+ * @s: pointer
+ * @b: constant
+ * @n: byte to use
+ * Return: pointeer s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int it;
+for (it = 0; n > 0; it++, n--)
+{
+s[it] = b;
+}
+return (s);
+}
