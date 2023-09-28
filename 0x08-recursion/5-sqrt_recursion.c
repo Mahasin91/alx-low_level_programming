870e89b229d4e492193430891a9976fffe837e8d
Mahasin91 <mahasinmoawad@gmail.com>
2023-09-28 17:52:15 -0400

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,11 +9,11 @@
  */
 int sqrt2(int a, int b)
 {
-	if (b * b == a)
-		return (b);
-	else if (b * b > a)
-		return (-1);
-	return (sqrt2(a, b + 1));
+if (b * b == a)
+return (b);
+else if (b * b > a)
+return (-1);
+return (sqrt2(a, b + 1));
 }
 /**
  * _sqrt_recursion - sqrt of num
@@ -23,5 +23,5 @@ int sqrt2(int a, int b)
 
 int _sqrt_recursion(int n)
 {
-	return (sqrt2(n, 1));
+return (sqrt2(n, 1));
 }
