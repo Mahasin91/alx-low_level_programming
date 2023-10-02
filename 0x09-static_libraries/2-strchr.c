a43132a9e32785c274b88f625cb67c713896179b
Mahasin91 <mahasinmoawad@gmail.com>
2023-10-02 16:04:45 -0400

    commitarch

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strchr - copy from string array
+ * @s: pointer
+ * @c: constant
+ * Return: pointer s
+*/
+
+char *_strchr(char *s, char c)
+{
+unsigned int it;
+for (it = 0; s[it] >= '\0'; it++)
+{
+if (s[it] == c)
+{
+return (s + it);
+}
+}
+return ('\0');
+}
