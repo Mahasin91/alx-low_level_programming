4f43466ef1f871c2727f3e92d831520af099e3be
Mahasin91 <mahasinmoawad@gmail.com>
2023-10-04 17:25:01 -0400

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,35 +9,35 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1, len2, len;
+	int i, j, ln1, ln2, ln;
 	char *result;
 
-	len1 = len2 = 0;
+	ln1 = ln2 = 0;
 
 	if (s1 != NULL)
 	{
 		i = 0;
 		while (s1[i++] != '\0')
-			len1++;
+			ln1++;
 	}
 
 	if (s2 != NULL)
 	{
 		i = 0;
 		while (s2[i++] != '\0')
-			len2++;
+			ln2++;
 	}
 
-	len = len1 + len2;
-	result = (char *)malloc(sizeof(char) * (len + 1));
+	ln = ln1 + ln2;
+	result = (char *)malloc(sizeof(char) * (ln + 1));
 	if (result == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
+	for (i = 0; i < ln1; i++)
 		result[i] = s1[i];
-	for (j = 0; j < len2; j++, i++)
+	for (j = 0; j < ln2; j++, i++)
 		result[i] = s2[j];
-	result[len] = '\0';
+	result[ln] = '\0';
 
 	return (result);
 }
