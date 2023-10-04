a3f3c8825cfd19d4bf9a8763544a025838e8fcec
Mahasin91 <mahasinmoawad@gmail.com>
2023-10-04 17:47:02 -0400

    commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -27,14 +27,13 @@ int number(char *str)
  * free_everything - frees the memory
  * @string: pointer values being passed for freeing
  * @i: counter
- * Return: nothing
  */
 
 void free_everything(char **string, int i)
 {
-	for (; i > 0;)
-		free(string[--i]);
-	free(string);
+for (; i > 0;)
+free(string[--i]);
+free(string);
 }
 
 /**
@@ -45,44 +44,47 @@ void free_everything(char **string, int i)
 
 char **strtow(char *str)
 {
-	int total_words = 0, b = 0, c = 0, length = 0;
-	char **words, *found_word;
+int total_words = 0, b = 0, c = 0, length = 0;
+char **words, *found_word;
 
-	if (str == 0 || *str == 0)
-		return (NULL);
-	total_words = number(str);
-	if (total_words == 0)
-		return (NULL);
-	words = malloc((total_words + 1) * sizeof(char *));
-	if (words == 0)
-		return (NULL);
-	for (; *str != '\0' &&  b < total_words;)
-	{
-		if (*str == ' ')
-			str++;
-		else
-		{
-			found_word = str;
-			for (; *str != ' ' && *str != '\0';)
-			{
-				length++;
-				str++;
-			}
-			words[b] = malloc((length + 1) * sizeof(char));
-			if (words[b] == 0)
-			{
-				free_everything(words, b);
-				return (NULL);
-			}
-			while (*found_word != ' ' && *found_word != '\0')
-			{
-				words[b][c] = *found_word;
-				found_word++;
-				c++;
-			}
-			words[b][c] = '\0';
-			b++; c = 0; length = 0; str++;
-		}
-	}
-	return (words);
+if (str == 0 || *str == 0)
+return (NULL);
+total_words = number(str);
+if (total_words == 0)
+return (NULL);
+words = malloc((total_words + 1) * sizeof(char *));
+if (words == 0)
+return (NULL);
+for (; *str != '\0' &&  b < total_words;)
+{
+if (*str == ' ')
+str++;
+else
+{
+found_word = str;
+for (; *str != ' ' && *str != '\0';)
+{
+length++;
+str++;
+}
+words[b] = malloc((length + 1) * sizeof(char));
+if (words[b] == 0)
+{
+free_everything(words, b);
+return (NULL);
+}
+while (*found_word != ' ' && *found_word != '\0')
+{
+words[b][c] = *found_word;
+found_word++;
+c++;
+}
+words[b][c] = '\0';
+b++;
+c = 0;
+length = 0;
+str++;
+}
+}
+return (words);
 }
