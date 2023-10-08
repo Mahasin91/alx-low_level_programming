0c4ddcce79531c5d081eca8fe2e18cb9c9a1ec05
Mahasin91 <mahasinmoawad@gmail.com>
2023-10-08 08:38:48 -0400

    commit

---
+++ 0x0C-more_malloc_free/c1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * string_nconcat - Function to concate two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * @n: num of byte from s2 to concate
+ * Return: pointer
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int l, i;
+	unsigned int j;
+	char *p;
+
+	l = 0;
+	i = 0;
+	j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[l])
+		l += 1;
+	p = malloc(l + n + 1);
+	if (p == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		p[i] = s1[i];
+		i += 1;
+	}
+
+	while (j < n)
+	{
+		p[i + j] = s2[j];
+		j += 1;
+	}
+	p[i + j] = '\0';
+	return (p);
+}
