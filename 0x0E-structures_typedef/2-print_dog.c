f0550dfb23f1609cd12c415cbd41166912517be0
Mahasin91 <mahasinmoawad@gmail.com>
2023-10-11 12:32:34 -0400

    commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - print instruct elem
+ * @d: dog to print
+ * Return: void
+*/
+
+void print_dog(struct dog *d)
+{
+if (d)
+{
+printf("name: %s\n", d->name ? d->name : "(nil)");
+printf("name: %s\n", d->age);
+printf("name: %s\n", d->owner ? d->owner : "(nil)");
+}
+}
