c80e8447c806e2cdf0345f81e62de0d8206f82a8
Mahasin91 <mahasinmoawad@gmail.com>
2023-10-11 13:10:12 -0400

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,10 +15,8 @@ char *name;
 float age;
 char *owner;
 };
+
 typedef struct dog dog_t;
-{
-    /* data */
-};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
