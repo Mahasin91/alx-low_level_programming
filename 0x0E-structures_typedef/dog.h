952b18135ca286936f70a462581cf84b4e0de4d4
Mahasin91 <mahasinmoawad@gmail.com>
2023-10-11 12:12:38 -0400

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_h
+
+/**
+ * struct dog - struct
+ * @name: name
+ * @age: age
+ * @owner: ownerś name
+ * 
+ * Description: dog struct
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
