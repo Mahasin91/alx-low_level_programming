4a5f4895e37155a4ba6d756cdf7730a94609b15d
Mahasin91 <mahasinmoawad@gmail.com>
2023-11-17 17:39:30 -0500

    commit

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copy from string array
+ * @dest: memory space
+ * @src: source
+ * @n: byte to use
+ * Return: pointer dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int it;
+for (it = 0; it < n; it++)
+{
+dest[it] = src[it];
+}
+return (dest);
+}
